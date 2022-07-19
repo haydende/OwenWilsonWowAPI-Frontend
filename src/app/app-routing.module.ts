e2d8bec7945b56eecd252e728d4315bee6bbc7e7
haydende <haydeneastwell@outlook.com>
2022-07-19 15:21:56 +0100

    Added routing for RandomWowComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RandomWowComponent } from './random-wow/random-wow.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'random', component: RandomWowComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
