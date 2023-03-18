b8e6e68c78b626f5b839549fd9e1b2be3bba4abb
haydende <haydeneastwell@outlook.com>
2023-03-18 18:12:53 +0000

    Removed requirement for karma-chrome-launcher and karam-firefox-launcher in favour of developer using browser of choice for Karam test port

--- karma.conf.js
+++ karma.conf.js
@@ -7,8 +7,6 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
