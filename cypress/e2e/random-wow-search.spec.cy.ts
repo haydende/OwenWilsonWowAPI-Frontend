32fa02a5dd71660c46c18f537c2f6e4539b9cce3
haydende <haydeneastwell@outlook.com>
2023-04-10 15:50:37 +0100

    Updated default random-search e2e test with stub, validation of number of wows shown

--- cypress/e2e/random-wow-search.spec.cy.ts
+++ cypress/e2e/random-wow-search.spec.cy.ts
@@ -1,14 +1,27 @@
 describe('Random Wow Search', () => {
 
-  specify('As a user, I should get a list of quotes from a random search', () => {
+  specify('As a user, I should get a list of quotes from a random search with the default form entries', () => {
+
+    // Stub request with 5_2000_MeetTheParents_JayRoach.json
+    cy.intercept({
+      method: 'GET',
+      url: '/wows/random?results=5&year=2000',
+      hostname: 'owen-wilson-wow-api.onrender.com'
+    }, {
+      fixture: 'random/5_2000_MeetTheParents_JayRoach.json'
+    })
+
     cy.visit('/random')
     cy.get('div #wow-list').should('be.empty')
     cy.wait(1000)
 
-    cy.get('#submit > button').click()
+    cy.get('#submit-button-div > #submit-button').click()
     cy.wait(2000)
 
-    cy.get('div #wow-list').should('not.be.empty')
+    cy.get('div #wow-list')
+      .should('not.be.empty')
+      .children()
+        .should('have.length', 2);
 
   })
 })
