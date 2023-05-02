53fe15f8d8ebea443aa14c67c19becf2372a12d0
haydende <haydeneastwell@outlook.com>
2023-05-02 16:32:26 +0100

    [patch] Refactored first-time theme selection logic to address null that was applied in Chromium browsers on Linux

--- src/app/service/theme.service.ts
+++ src/app/service/theme.service.ts
@@ -31,7 +31,7 @@ export class ThemeService {
     private init() {
         const savedTheme = this.settingStorage.getSetting(Setting.THEME) as Theme;
         const prefersColorSchemeDark = window.matchMedia("(prefers-color-scheme: dark)");
-        let useDarkMode = (prefersColorSchemeDark.matches && savedTheme == null) || Theme.DARK == savedTheme
+        let useDarkMode = (prefersColorSchemeDark.matches || Theme.DARK == savedTheme) || savedTheme == null;
         let themeToApply: Theme = (useDarkMode) ? Theme.DARK : savedTheme;
 
         this.applyTheme(themeToApply);
